df722a4701c987df48a7fc9009bc190601eb3904
Felipe Carasso <felipe@carassonet.org>
2016-12-20 00:29:01 -0500

    Optimization...

--- p19-b.sh
+++ p19-b.sh
@@ -2,21 +2,32 @@
 
 NUMBER=$1
 
-PRESENTS=/tmp/p19.presents
+PRESENTS1=/tmp/p19.presents
 PRESENTS2=${PRESENTS}2
 
 if [ ! -f $PRESENTS ]; then
 	seq 1 $NUMBER > $PRESENTS
 fi
 
-while [ $((currentNumber = $(wc -l < $PRESENTS))) -gt 2 ]; do
+pNum=2
+pName=PRESENTS
+
+switch() {
+	p1=${pName}$((pNum = (pNum * 2)  % 3))
+	p2=${pName}$((3 - pNum))
+}
+
+switch
+
+while [ $((currentNumber = $(wc -l < ${!p1}))) -gt 2 ]; do
 	half=$((currentNumber/2))
 
-	sed "1{h;d};$((half+1))d;\${G}" < $PRESENTS > $PRESENTS2
+	sed "1{h;d};$((half+1))d;\${G}" ${!p1} > ${!p2}
 
-	mv $PRESENTS2 $PRESENTS
+	#mv $PRESENTS2 $PRESENTS
+	switch
 
-	(($currentNumber % 10 == 0 ? 1 : 0)) && echo -e "\r${currentNumber}...\c"
+	(($currentNumber % 1 == 0 ? 1 : 0)) && echo -e "\r${currentNumber}...\c"
 done
 
 echo
